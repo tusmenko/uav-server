21498bb84457c523a45cc2c09889619a8e87414a
Yevhenii Tusmenko <tusmey01@stepstone.com>
2022-09-11 19:49:39 +0200

    Setup heroku

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
   // await redisIoAdapter.connectToRedis();
   // app.useWebSocketAdapter(redisIoAdapter);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
